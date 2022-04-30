8c8e727cd1500e61f4367b4eb68236c1dba31d68
Ellen <ellen.deveth@hotmail.com>
2022-04-30 14:02:08 +0200

    feat: make frontend to choose an account

---
+++ register/register-user.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.typekit.net/hbs6zbg.css">
+    <link rel="stylesheet" href="../style/style.css">
+</head>
+<body class="body--startpage">
+    <h1 class="header-one">Welk type account wil je aanmaken?</h1>
+
+    <div class="container">
+        <div class= "container__block">
+            <div class="block__text">
+                <h2 class="header-two">Student</h2>
+                <p class="subtitle-big">Voor studenten die hun steentje willen bijdragen.</p>
+                <span class="text__list">
+                    <ul class="list">
+                        <li class="list__item">Gratis account aanmaken</li>
+                        <li class="list__item">Vind het geschikte project dat overeenkomt met jouw leerdoelen en competenties</li>
+                        <li class="list__item">Behaal leerdoelen terwijl je werkt aan echte opdrachten voor echte organisaties die jouw hulp nodig hebben</li>
+                    </ul>
+                </span>
+            </div>
+            
+            <div class="btn__block">
+                <a class="btn btn-register" href="">
+                    <span class="btn__text">Account aanmaken</span>
+                </a>
+            </div>
+            
+        </div>
+
+        <div class="container__block">
+            <div class="block__text">
+                    <h2 class="header-two">Organisatie</h2>
+                    <h3 class="subtitle-big">Voor organisaties waar alle hulp welkom is.</h3>
+                    <span class="text__list">
+                    <ul class="list">
+                        <li class="list__item">Gratis account aanmaken</li>
+                        <li class="list__item">Vind gemotiveerde studenten met de geschikte skills die jouw organisatie komen ondersteunen</li>
+                        <li class="list__item">Maak een project aan met de nodige informatie zodat het gevonden kan worden door de geschikte studenten</li>
+                    </ul>
+                </span>
+                </div>
+                
+                <div class="btn__block">
+                    <a class="btn btn-register" href="">
+                        <span class="btn__text">Account aanmaken</span>
+                    </a>
+                </div>
+        </div>
+    </div>
+</body>
+</html>
