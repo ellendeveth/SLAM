82c58df023a5c475c62018878e205331dd4a6c18
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-03 21:05:52 +0200

    refactor: add extra container div around competence blocks to make it possible to outline them to the left while also centering them in the middle of the page

--- register/competentions.php
+++ register/competentions.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,55 +9,59 @@
     <link rel="stylesheet" href="https://use.typekit.net/hbs6zbg.css">
     <link rel="stylesheet" href="../style/style.css">
 </head>
+
 <body class="body--startpage">
     <div class="container__comp">
         <div class="main">
             <h1 class="form__title">Competenties</h1>
             <span class="subtitle-big">Duid je vakgebied aan.</span>
         </div>
-        
-        <div class="block">
-            <a class="block__link" href="">
-                <span class="block__text">Business</span>
-                <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
-            </a>
-        </div>
 
-        <div class="block">
-            <a class="block__link" href="">
-                <span class="block__text">Design</span>
-                <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
-            </a>
-        </div>
+        <div class="container__blocks">
+            <div class="block">
+                <a class="block__link" href="">
+                    <span class="block__text">Business</span>
+                    <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
+                </a>
+            </div>
 
-        <div class="block">
-            <a class="block__link" href="">
-                <span class="block__text">Event en organisatie</span>
-                <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
-            </a>
-        </div>
+            <div class="block">
+                <a class="block__link" href="">
+                    <span class="block__text">Design</span>
+                    <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
+                </a>
+            </div>
 
-        <div class="block">
-            <a class="block__link" href="">
-                <span class="block__text">IT en development</span>
-                <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
-            </a>
-        </div>
+            <div class="block">
+                <a class="block__link" href="">
+                    <span class="block__text">Event en organisatie</span>
+                    <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
+                </a>
+            </div>
 
-        <div class="block">
-            <a class="block__link" href="">
-                <span class="block__text">Marketing en communicatie</span>
-                <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
-            </a>
-        </div>
+            <div class="block">
+                <a class="block__link" href="">
+                    <span class="block__text">IT en development</span>
+                    <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
+                </a>
+            </div>
 
-        <div class="block">
-            <a class="block__link" href="">
-                <span class="block__text">Onderwijs</span>
-                <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
-            </a>
+            <div class="block">
+                <a class="block__link" href="">
+                    <span class="block__text">Marketing en communicatie</span>
+                    <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
+                </a>
+            </div>
+
+            <div class="block">
+                <a class="block__link" href="">
+                    <span class="block__text">Onderwijs</span>
+                    <img class="block__img" src="../assets/img/arrow.png" alt="arrow">
+                </a>
+            </div>
         </div>
     </div>
-    
+
 </body>
+
 </html>
