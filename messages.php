7cf716464892d160812aceb9e0de2a3d823d200a
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-10 21:32:36 +0200

    feat: make messages page

---
+++ messages.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Berichten</title>
+    <link rel="stylesheet" href="https://use.typekit.net/hbs6zbg.css">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+</head>
+<body>
+    <div class="container--messages">
+        <div class="messages__groups">
+            <div class="messages__chat__info">
+                <img class="chat__info__img" src="assets/img/arrow.png" alt="arrow">
+                <h2>Chats</h2>
+            </div>
+
+            <div>
+                <input class="messages__chat__search" type="text" placeholder="Search">
+            </div>
+
+            <div class="messages__chat__groups">
+                <h2>Groepen</h2>
+                <div class="chat__groups">
+                    <img class="projects__img" src="assets/img/profile-pic.png" alt="profile-pic">
+                    <div class="chat__groups__text">
+                        <h3 class="subtitle-big">Groep 1</h3>
+                        <p>kakakaka</p>
+                    </div>                    
+                </div>
+                <div class="chat__groups">
+                    <img class="projects__img" src="assets/img/profile-pic.png" alt="profile-pic">
+                    <div class="chat__groups__text">
+                        <h3 class="subtitle-big">Groep 2</h3>
+                        <p>pipipipi</p>
+                    </div>                    
+                </div>
+                <div class="chat__groups">
+                    <img class="projects__img" src="assets/img/profile-pic.png" alt="profile-pic">
+                    <div class="chat__groups__text">
+                        <h3 class="subtitle-big">Groep 3</h3>
+                        <p>hihihihi</p>
+                    </div>                    
+                </div>
+            </div>
+        </div>
+
+        <div class="messages__chat">
+            <div class="messages__chat__emptystate">
+                <img class="chat__emptystate__img" src="assets/img/emptystate-messages.png" alt="empty state">
+                <h2 class="header-two">Je hebt nog geen berichtjes</h2>
+                <p>Doe mee aan een project om in een groep te sturen.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
