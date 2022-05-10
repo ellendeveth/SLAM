35addedad5baaf80af764fc598a3b57b0c82e468
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-10 21:32:49 +0200

    feat: make reset password page

---
+++ forgot-password.php
@@ -0,0 +1,32 @@
+<?php
+  
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.typekit.net/hbs6zbg.css">
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body class="body--startpage">
+    <div class="form">
+        <form action="" method="post">
+            <h1 class="form__title">Reset je wachtwoord</h1>
+            <p class="alert-message">Er wordt een mail verstuurd om je wachtwoord te resetten</p>
+            
+            <div class="form__container">
+                <label class="form__text" for="email">Email</label>
+                <input class="form__input" type="text" name="email">
+            </div>
+            
+            <div >
+                <input class="btn" type="submit" name="reset" value="Verstuur mail">
+            </div>
+
+        </form>
+    </div>
+    
+</body>
+</html>
