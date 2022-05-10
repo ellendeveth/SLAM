780c0af5a79a1373144c4fb3e257e7fbbdecd236
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-05-10 21:32:23 +0200

    feat: make index page

--- index.php
+++ index.php
@@ -10,5 +10,91 @@
 </head>
 <body>
     <?php require_once('header.php'); ?>
+
+    <div class="container--feed">
+        <div class="filters">
+            <div class="filters__language">
+                <h2>Taal van project</h2>
+                <div class="filter__languages">
+                    <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="language" value="nl">
+                        <label for="language">Nederlands</label>
+                    </div>
+                    <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="language" value="fr">
+                        <label for="language">Frans</label>
+                    </div>
+                    <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="language" value="en">
+                        <label for="language">Engels</label>
+                    </div>
+                </div>
+            </div>
+
+            <div class="filters__skills">
+                <h2>Vaardigheden</h2>
+                <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="skills" value="com">
+                        <label class="filter__checkbox--active" for="skills">Communicatie</label>
+                    </div>
+                    <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="skills" value="inf">
+                        <label for="skills">Informatica</label>
+                    </div>
+                    <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="skills" value="des">
+                        <label for="skills">Webdesign</label>
+                    </div>
+                    <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="skills" value="ond">
+                        <label for="skills">Onderwijs</label>
+                    </div>
+                    <div>
+                        <input class="filter__checkbox--active" type="checkbox" name="skills" value="org">
+                        <label for="skills">Organisatie</label>
+                    </div>
+            </div>
+            <div>
+                <input class="link" type="submit" name="submit" value="+ Meer opties">
+            </div>
+        </div>
+
+        <div class="projects">
+            <h1 class="header-two">Projecten</h1>
+            <div class="projects__box">
+                <ul class="project__box__li">
+                    <!-- foreach ($projects as $project):  -->
+                    <li>
+                        <div class="project__box">
+                            <div class="project__box__profile">
+                                <img class="projects__img" src="assets/img/profile-pic.png" alt="profile-pic">
+                                <h2>Project 1</h2>
+                            </div>
+
+                            <div class="project__box__item">
+                                <div>
+                                    <h3>Titel project</h3>
+                                    <ul class="project__tags">
+                                        <li class="tags__tag">tag</li>
+                                        <li class="tags__tag">tag</li>
+                                        <li class="tags__tag">tag</li>
+                                    </ul>
+                                    <p>beschrijving</p>
+                                </div>
+                                
+                                <div class="project__info">
+                                    <a class="project__info__link" href="">Meer info</a>
+                                    <img class="project__info__img" src="assets/img/arrow.png" alt="arrow">
+                                </div>
+                            </div>
+
+                        </div>
+                    </li>
+                    <!--  endforeach; -->
+                </ul>
+            </div>
+        </div>
+    </div>
+
 </body>
 </html>
