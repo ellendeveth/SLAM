1a350464e5f71c0358ec445f824c427c59885dfb
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-11 17:01:27 +0200

    feat: add logo and navigation icons

--- header.php
+++ header.php
@@ -1,17 +1,35 @@
 <nav class="nav">
     <div class="nav__logo">
         <a href="">
-            <img src="" alt="">
-            <span>logo</span>
+            <img src="./assets/img/logo.svg" alt="">
         </a>
     </div>
+
     <div class="nav__searchbar">
         <input class="searchbar__input" type="text" placeholder="Zoeken">
         <button class="searchbar__submit">Zoeken</button>
     </div>
-    <div class="nav__links">
-        <a href="">Projecten</a>
-        <a href="">Berichten</a>
-        <a href="">Instellingen</a>
+
+    <div class="nav__links__container">
+        <div class="nav__links">
+            <a href="">
+                <img src="./assets/img/projects.svg" alt="">
+                <span class="nav__links__text">Projecten</span>
+            </a>
+        </div>
+
+        <div class="nav__links">
+            <a href="">
+                <img src="./assets/img/messages.svg" alt="">
+                <span class="nav__links__text">Berichten</span>
+            </a>
+        </div>
+
+        <div class="nav__links">
+            <a href="">
+                <img src="./assets/img/settings.svg" alt="">
+                <span class="nav__links__text">Instellingen</span>
+            </a>
+        </div>
     </div>
 </nav>
