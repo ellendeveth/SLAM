22efbb62e71aaa3c3f1af31e2b972b981337564e
Alejandro De Wolf <dewolf.alejandro@gmail.com>
2022-05-14 18:20:42 +0200

    feat: page the user gets redirected to if he signs up for a projects

---
+++ confirmation.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Berichten</title>
+        <link rel="stylesheet" href="https://use.typekit.net/hbs6zbg.css">
+        <link rel="stylesheet" href="style/style.css">
+    </head>
+</head>
+
+<body>
+    <?php require_once('header.php'); ?>
+
+
+    <div class="messages__chat">
+        <div class="messages__chat__emptystate confirmation__emptystate">
+            <img class="chat__emptystate__img" src="assets/img/confirmation.svg" alt="empty state">
+            <h2 class="header-two">Aanmelding succesvol!</h2>
+            <p>Jouw aanmelding om mee te doen aan het project wordt momenteel gecontroleerd.
+                Houd jouw e-mail inbox zeker goed in het oog, je mag spoedig een mail verwachten!</p>
+            <a href="" class="btn btn__anchor">Naar Dashboard</a>
+        </div>
+    </div>
+</body>
+
+</html>
