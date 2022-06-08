53bbd883790d6222b18139ba04d0198891087fda
ellendeveth <94909247+ellendeveth@users.noreply.github.com>
2022-06-09 00:16:46 +0200

    fix: change filename for local use

--- classes/Db.php
+++ classes/Db.php
@@ -10,7 +10,7 @@
                 return self::$conn;
             } else {
                 // New connection
-                $config = parse_ini_file($_SERVER["DOCUMENT_ROOT"] . "/slam/config/config.ini");
+                $config = parse_ini_file($_SERVER["DOCUMENT_ROOT"] . "/SLAM/config/config.ini");
                 self::$conn = new PDO('mysql:host='. $config['db_host'] .';dbname=' . $config['db_name'] . ";charset=utf8mb4", $config['db_user'], $config['db_password']);
                 return self::$conn;
             }
